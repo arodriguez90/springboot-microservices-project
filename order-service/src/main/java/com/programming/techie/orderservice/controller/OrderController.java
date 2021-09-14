b909238465d0088fe7f411332e38e0ad64c7d3d3
Armando Rodriguez <arodriguez_90@outlook.com>
2021-09-14 16:05:47 -0500

    Initialize notification service

--- order-service/src/main/java/com/programming/techie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programming/techie/orderservice/controller/OrderController.java
@@ -6,9 +6,12 @@ import com.programming.techie.orderservice.model.Order;
 import com.programming.techie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api/order")
@@ -18,11 +21,16 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
     @PostMapping
     public String placeOrder(@RequestBody OrderDto orderDto){
-        boolean allProductsInStock = orderDto.getOrderLineItemsList().stream()
-                .allMatch(orderLineItems -> inventoryClient.checkStock(orderLineItems.getSkuCode()));
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+
+        Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItemsList().stream()
+                .allMatch(lineItem -> inventoryClient.checkStock(lineItem.getSkuCode()));
+
+        boolean allProductsInStock = circuitBreaker.run(booleanSupplier, throwable -> handleErrorCase());
 
         if(allProductsInStock){
             Order order = new Order();
@@ -35,5 +43,9 @@ public class OrderController {
         }else{
             return "Order Failed, One of the products in the order is not in stock";
         }
+    }
+
+    private Boolean handleErrorCase(){
+        return false;
     }
 }
